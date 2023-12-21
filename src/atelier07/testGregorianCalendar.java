68ab1d9d9832ead85ee1558900bdc4963a8e7f7f
isha <isha@fedora>
2023-12-21 10:37:32 +0100

    .

---
+++ src/atelier07/testGregorianCalendar.java
@@ -0,0 +1,15 @@
+package atelier07;
+
+import java.util.*;
+
+public class testGregorianCalendar {
+
+	public static void main(String[] args) {
+		GregorianCalendar gc = new GregorianCalendar();
+		
+		Date d = gc.getTime();
+		System.out.println(d.toString());
+
+	}
+
+}
